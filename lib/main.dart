cf6350549ca855b603c5c799d9d0c334ac14a6d4
Nguyen-Van-Truong <20130187@st.hcmuaf.edu.vn>
2024-06-07 06:21:41 +0700

    Đổi tên app thành Doralingo

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'English App',
+      title: 'Doralingo ',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
